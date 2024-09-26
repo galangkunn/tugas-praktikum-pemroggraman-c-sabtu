19c178cefc48c0568dc141df1e0b1a2c51569d03
galangkunn <putragalang727@gmail.com>
2024-09-26 13:34:44 +0700

    first-commit

---
+++ tugas-praktikum-c-sabtu/tugas-mandiri-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("NIM : 2441019\n");
+  printf("Nama : Galang Putra Pratama\n");
+  printf("Jurusan : Teknik Informatika");
+  return 0;
+}
---
+++ tugas-praktikum-c-sabtu/tugas-mandiri-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int p,l,ls;
+  p = 10;
+  l = 7;
+  ls = p*l;
+  printf("Panjang dari persegi panjang tersebut adalah %i cm\n",p);
+  printf("Lebar dari persegi panjang tersebut adalah %i cm\n",l);
+  printf("Luas dari persegi panjang tersebut adalah %i cm\n",ls);
+}
